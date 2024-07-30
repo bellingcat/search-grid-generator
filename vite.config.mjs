8f1b36e997a223e1bfddc9d34d011c7b8e3aed3a
Galen Reich <galen@bellingcat.com>
2024-07-30 19:06:16 +0100

    Fix base url in vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,6 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/search-grid-generator/',
   plugins: [
     VueRouter(),
     Vue({
