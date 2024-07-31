51ab852833daf726a4298b945a58f30f32ab74c0
Logan Williams <logan.williams@alum.mit.edu>
2024-07-31 14:12:37 +0200

    Change to relative base URL

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/search-grid-generator/',
+  base: './',
   plugins: [
     VueRouter(),
     Vue({
