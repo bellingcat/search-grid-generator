cc637581d617fcf8ea4a3a7873ba8ad55acc332c
Galen Reich <galen@bellingcat.com>
2024-08-01 18:37:49 +0100

    Increase compactness

--- src/components/GridGenerator.vue
+++ src/components/GridGenerator.vue
@@ -51,22 +51,28 @@
         <v-card class="mx-auto" max-width="600">
           <v-card-text>
             <v-form v-model="isFormValid">
+              <v-row class="px-3 pb-5 pt-1">
               <v-text-field
+                class="pr-1"
                 v-model="coordinates1"
                 label="Coordinate 1 (lat, lon)"
                 placeholder="48.90529373066811, 2.2584719879516464"
                 outlined
                 :rules="[(v) => !!v]"
                 density="compact"
+                hide-details="true"
               ></v-text-field>
               <v-text-field
+              class="pl-1"
                 v-model="coordinates2"
                 label="Coordinate 2 (lat, lon)"
                 placeholder="48.81418021233939, 2.4172392962309246"
                 outlined
                 :rules="[(v) => !!v]"
                 density="compact"
+                hide-details="true"
               ></v-text-field>
+            </v-row>
               <v-text-field
                 v-model.number="gridSize"
                 label="Grid Size (km)"
